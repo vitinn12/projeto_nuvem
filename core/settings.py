6af5beb544807ca8e3bf9d8d8554a339e088872c
vitinn12 <vitoralves1212.va@gmail.com>
2026-02-12 18:40:01 -0300

    ajustando o settings

--- core/settings.py
+++ core/settings.py
@@ -130,3 +130,5 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CSRF_TRUSTED_ORIGINS = ['https://projeto-nuvem-vzwf.onrender.com']
